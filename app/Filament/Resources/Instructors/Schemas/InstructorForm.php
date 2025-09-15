1fc5adfe970d32f993a5409a80c4af1666c5c09d
Faishal Fariz <faishalfh1.work@gmail.com>
2025-09-15 13:39:25 +0700

    Add Instructor Features V1

---
+++ app/Filament/Resources/Instructors/Schemas/InstructorForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\Instructors\Schemas;
+
+use Filament\Schemas\Schema;
+
+class InstructorForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Instructors/Tables/InstructorsTable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filament\Resources\Instructors\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ForceDeleteBulkAction;
+use Filament\Actions\RestoreBulkAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+
+
+class InstructorsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')
+                    ->label('ID')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('nama')
+                    ->label('Nama')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('email')
+                    ->label('Email')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('total_hari_ajar')
+                    ->label('Total Hari Ajar')
+                    ->sortable()
+                    ->searchable(),
+                ])
+            ->filters([
+                TrashedFilter::make(),
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+}
